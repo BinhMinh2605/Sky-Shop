19c76384d327c0d7309dfb21a0342fe2dba7fa0e
SunriseBM <58320732+SunriseBM@users.noreply.github.com>
2021-01-17 21:41:48 -0800

    firt commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sky_shop.Startup))]
+namespace sky_shop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
